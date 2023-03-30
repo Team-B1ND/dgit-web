60af228a17006ae0d0a7a9e372b14dbde5c9b892
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-03-30 12:01:33 +0900

    피드백 반영(스타일링 코드 분기처리)

--- src/components/common/Avatar/index.tsx
+++ src/components/common/Avatar/index.tsx
@@ -3,13 +3,12 @@ import { AvatarProps } from "./types";
 
 const Avatar = ({ customStyle, src, isHexagonFrame }: AvatarProps) => {
   return (
-    <S.ProfileContainer onClick={() => window.open("https://github.com/")}>
-      {isHexagonFrame === true ? (
-        <S.HexagonProfile style={{ ...customStyle }} src={src} />
-      ) : (
-        <S.CircleProfile style={{ ...customStyle }} src={src} />
-      )}
-    </S.ProfileContainer>
+    <S.GitProfile
+      style={{ ...customStyle }}
+      src={src}
+      onClick={() => window.open("https://github.com/")}
+      isHexagonFrame={isHexagonFrame}
+    />
   );
 };
 
