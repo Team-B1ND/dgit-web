d208dc7287c246bed1e78acbb6826d6f7571b37c
전해림 <HAERIM060920@gmail.com>
2023-05-16 21:00:44 +0900

    ♻ :: 모달 수정

--- src/repositories/auth/AuthRepositoryImpl.ts
+++ src/repositories/auth/AuthRepositoryImpl.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { TokenResponse } from "../../types/token/Token.type";
 import { AuthRepository } from "./AuthRepository";
+// import config from "../../config/config.json";
 import config from "../../config/config.json";
 
 class AuthRepositoryImpl implements AuthRepository {
