23a447f587c6d1d1957ad1181e227e097f34174e
ftery0 <lerb5253@dgsw.hs.kr>
2025-03-12 17:30:20 +0900

    feat :: refresh

---
+++ src/lib/axios/responseHandler.ts
@@ -0,0 +1,31 @@
+import { AxiosError } from "axios";
+import {
+  ACCESS_TOKEN_KEY,
+  REFRESH_TOKEN_KEY,
+} from "constants/token/token.constant";
+import AuthRepositoryImpl from "repositories/auth/AuthRepositoryImpl";
+import token from "../token/token";
+
+export const responseHandler = async (error: AxiosError) => {
+  if (error.response) {
+    const {
+      response: { status },
+    } = error;
+
+    const usingAccessToken = token.getToken(ACCESS_TOKEN_KEY);
+    const usingRefreshToken = token.getToken(REFRESH_TOKEN_KEY);
+
+    if (
+      usingAccessToken !== undefined &&
+      usingRefreshToken !== undefined &&
+      status === 401
+    ) {
+      try {
+        const { data: newAccessToken } = await AuthRepositoryImpl.refresh(
+          usingAccessToken
+        );
+        token.setToken(ACCESS_TOKEN_KEY, newAccessToken.accessToken);
+      } catch (error) {}
+    }
+  }
+};
